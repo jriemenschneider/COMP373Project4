0b9c1240bf255059348bb2495ce57a8d507b949c
Bree <broberts2@luc.edu>
2020-04-24 01:55:43 -0500

    Builder class, client is not finished yet

--- src/model/view/Client.java
+++ src/model/view/Client.java
@@ -10,7 +10,11 @@ public class Client {
         double taxTotal = iTC.calcTax(items);//gets total of all items
         System.out.println("Total with tax added: " + taxTotal);
     } 
-	//OTHER PATTERN
+	
+	public static void main(String [] args) {
+		//builder
+		
+	}
 	
 }
 	
