50febb80c97008686cdd6307d3e0fa268e0ffca6
Jaimie Riemenschneider <jriemenschneider@luc.edu>
2020-04-24 13:39:48 -0400

    Did some testing

--- Test/model/test/TestVisitor.java
+++ Test/model/test/TestVisitor.java
@@ -8,9 +8,10 @@ class TestVisitor {
 
 	@Test
 	void testAddItem() {
+		//test to see if the add item in ordercart works
 		
 	}
-	
+	//see if the total with tax works each time and maybe do mutliple versions of the client?(like multiple versions of adding different items?(Do i need to add a remove item?
 	
 
 }
